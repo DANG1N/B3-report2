38da214de545af7d7f98cacbfcfebcce9edd846a
Will <will@experimentalgeek.com>
2024-03-12 19:28:21 +0000

    made a build powershell script - look at build.ps1 for info

---
+++ build.ps1
@@ -0,0 +1,20 @@
+<# This script creates compiles the passed .tex file and places it in the PDF directory
+for main.tex run with:
+.\build.ps1
+
+for other.tex run with
+.\build.ps1 -file other
+#>
+
+param(
+    [string]$file = "main"
+)
+
+pdflatex $file
+biber $file
+pdflatex $file
+pdflatex $file
+$FROM = "$file.pdf"
+$TO = "PDF/$file.pdf"
+del $TO
+mv $FROM $TO
