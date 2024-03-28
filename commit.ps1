67d4cdea3b8775d6b73d12a4577d6b7fd2ab8026
Will <will@experimentalgeek.com>
2024-03-28 16:33:06 +0000

    automated commit: yo

---
+++ commit.ps1
@@ -0,0 +1,15 @@
+<# This script creates automatically adds, commits and pushes to the chosen branch #>
+
+param(
+    [string]$msg = "default", [string]$branch = "main"
+)
+
+git add .
+git status
+git commit -m "automated commit: $msg"
+$confirmation = Read-Host "Happy? [y/n]"
+while ($confirmation -ne "y") {
+    if ($confirmation -eq 'n') { exit }
+    $confirmation = Read-Host "Ready? [y/n]"
+}
+git push -u origin $branch
